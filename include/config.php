32da673ffc22792af5388d8670af850bf791b07e
urossimic <simic.uros@gmail.com>
2017-07-01 12:04:38 +0200

    added db name in config

--- include/config.php
+++ include/config.php
@@ -2,5 +2,5 @@
 define('DB_USERNAME', 'qgbjcrqq_paw'); // todo db user
 define('DB_PASSWORD', 'dps;ro([2=v$z[yvE?'); //todo db user pass
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'qgbjcrqq_petwork_dev'); // todo db name
+define('DB_NAME', 'qgbjcrqq_hackaton');
 ?>
