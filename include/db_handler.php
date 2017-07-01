ed125a56c5d74677030e7ca5ef441bf4420e469d
urossimic <simic.uros@gmail.com>
2017-07-01 15:48:25 +0200

    implemented basic api calls

--- include/db_handler.php
+++ include/db_handler.php
@@ -14,7 +14,10 @@ class DbHandler {
     }
 
     public function getLocationData($user_id) {
-      $stmt = $this->conn->prepare("SELECT * FROM location WHERE user_id = ?");
+      $stmt = $this->conn->prepare("SELECT *
+        FROM location
+        WHERE user_id = ?
+        ORDER BY created_at DESC");
 
       $stmt->bind_param("s", $user_id);
       $stmt->execute();
@@ -24,13 +27,61 @@ class DbHandler {
     }
 
     public function getDeviceData($user_id) {
-      $stmt = $this->conn->prepare("SELECT * FROM device_data WHERE user_id = ?");
+      $stmt = $this->conn->prepare("SELECT *
+        FROM device_data
+        WHERE user_id = ?
+        ORDER BY created_at DESC
+        LIMIT 10");
 
       $stmt->bind_param("s", $user_id);
       $stmt->execute();
       $result = $stmt->get_result();
       $stmt->close();
       return $result;
+    }
+
+    public function setLocation($user_id, $latitude, $longitude, $address, $city, $state, $country, $postal_code, $known_address, $time_stamp) {
+        $response = array();
+        //subscribe to itself
+        $stmt = $this->conn->prepare("INSERT INTO location
+        (user_id, latitude, longitude, address, city, state, country, postal_code, known_address, time_stamp)
+        VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssssssss", $user_id, $latitude, $longitude, $address, $city, $state, $country, $postal_code, $known_address, $time_stamp);
+
+        $result = $stmt->execute();
+        $stmt->close();
+
+        if($result) {
+          $response["error"] = 0;
+          $response["error_message"] = "success";
+        } else {
+          // Failed to create user
+          $response["error"] = 4444;
+          $response["error_message"] = "Failed to insert new location data";
+        }
+        return $response;
+    }
+
+    public function setDevice($user_id, $imei, $phone_number, $phone_model, $os) {
+        $response = array();
+        //subscribe to itself
+        $stmt = $this->conn->prepare("INSERT INTO device_data
+        (user_id, imei, phone_number, phone_model, os)
+        VALUES(?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssss", $user_id, $imei, $phone_number, $phone_model, $os);
+
+        $result = $stmt->execute();
+        $stmt->close();
+
+        if($result) {
+          $response["error"] = 0;
+          $response["error_message"] = "success";
+        } else {
+          // Failed to create user
+          $response["error"] = 4444;
+          $response["error_message"] = "Failed to insert new location data";
+        }
+        return $response;
     }
 // -----------------------------------------------//
 
--- v1/index.php
+++ v1/index.php
@@ -33,6 +33,7 @@
                   $tmp["country"] = $row["country"];
                   $tmp["postal_code"] = $row["postal_code"];
                   $tmp["known_address"] = $row["known_address"];
+                  $tmp["time_stamp"] = $row["time_stamp"];
                   $tmp["created_at"] = $row["created_at"];
 
                   array_push($response["locations"], $tmp);
@@ -40,6 +41,68 @@
               }
 
               echoRespnse(200, $response);
+    });
+
+    $app->get('/getDeviceData',  function() use ($app) {
+              $user_id = $app->request->get('user_id');
+
+              $db = new DbHandler();
+              $result = $db->getDeviceData($user_id);
+
+              if(is_null($result)){
+                $response["error"] = "no results";
+              }else{
+                $response["devices"] = array();
+                // pushing single location into array
+                while ($row = $result->fetch_assoc()) {
+                  $tmp = array();
+                  $tmp["device_data_id"] = $row["id"];
+                  $tmp["user_id"] = $row["user_id"];
+                  $tmp["imei"] = $row["imei"];
+                  $tmp["phone_number"] = $row["phone_number"];
+                  $tmp["phone_model"] = $row["phone_model"];
+                  $tmp["os"] = $row["os"];
+                  $tmp["created_at"] = $row["created_at"];
+
+                  array_push($response["devices"], $tmp);
+                }
+              }
+              echoRespnse(200, $response);
+    });
+
+    $app->post('/location', function() use ($app) {
+               // reading post params
+               $user_id = $app->request->post('user_id');
+               $latitude = $app->request->post('latitude');
+               $longitude = $app->request->post('longitude');
+               $address = $app->request->post('address');
+               $city = $app->request->post('city');
+               $state = $app->request->post('state');
+               $country = $app->request->post('country');
+               $postal_code = $app->request->post('postal_code');
+               $known_address = $app->request->post('known_address');
+               $time_stamp = $app->request->post('time_stamp');
+
+               $db = new DbHandler();
+               $response = $db->setLocation($user_id, $latitude, $longitude, $address, $city, $state, $country, $postal_code, $known_address, $time_stamp);
+
+               // echo json response
+               echoRespnse(200, $response);
+    });
+
+    $app->post('/device', function() use ($app) {
+               // reading post params
+               $user_id = $app->request->post('user_id');
+               $imei = $app->request->post('imei');
+               $phone_number = $app->request->post('phone_number');
+               $phone_model = $app->request->post('phone_model');
+               $os = $app->request->post('os');
+
+               $db = new DbHandler();
+               $response = $db->setDevice($user_id, $imei, $phone_number, $phone_model, $os);
+
+               // echo json response
+               echoRespnse(200, $response);
     });
 
     //----------------------------------------------------//
